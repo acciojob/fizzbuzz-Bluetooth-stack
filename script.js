c0f7a1b6b4cdfec41f8a78edbd380e5b1ebd90b4
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-29 13:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let str = "";
+for(let i = 1; i<=20; i++){
+	if(i%3==0 && i%5==0){
+		str += 'FizzBuzz'+'\n';
+		// str += i + '\n';
+	}
+	else if(i%5==0){
+		str += 'Buzz'+'\n';
+		// str += i + '\n';
+	}
+	else if(i%3==0){
+		str += 'Fizz'+'\n';
+		// str += i + '\n';
+	}
+}
+alert(str);
